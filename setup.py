cbe53d5ecbadf916f42c28a4e6c093c32df02bae
Daniel Boocock <daniel.boocock@protonmail.ch>
2017-01-22 21:36:08 +0000

    Reorganize setup

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 
-setup(name="cans",
-      version="0.1",
-      description="Reaction-diffusion model for QFA",
-      long_description=readme(),
-      url="https://github.com/boo62/cans.git",
-      author="Daniel Boocock",
-      author_email="daniel.boocock@protonmail.ch",
-      license="",
-      packages=["cans"],
-      # package_dir={"cans"},
-      # py_modules=["cans.plate", "cans.model",
-      #             "cans.fitter", "cans.plotter"],
-      scripts=[],
-      install_requires=[],
-      test_suite="tests",
-      tests_require=["pytest"],
-      zip_safe=False)
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+setup(
+    name="cans",
+    version="0.1",
+    description="Reaction-diffusion model for QFA",
+    long_description=readme(),
+    url="https://github.com/boo62/cans.git",
+    author="Daniel Boocock",
+    author_email="daniel.boocock@protonmail.ch",
+    license="",
+    packages=["cans", "models"],
+    # scripts=[],
+    # install_requires=[],
+    # test_suite="tests",
+    # zip_safe=False,
+    setup_requires=["pytest-runner",],
+    tests_require=["pytest",],
+)

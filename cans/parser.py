daf88b49b57d79add92edf5ea4259e3908d430fe
Daniel Boocock <daniel.boocock@protonmail.ch>
2017-02-01 21:24:30 +0000

    Group fields and define processing

--- cans/parser.py
+++ cans/parser.py
@@ -5,32 +5,68 @@ import re
 
 class Parser(object):
 
-    _SBML_math_characters = r"\w\(\)"
+    def __init__(self, path):
+        self.path = path
+        self.parse(path)
 
-    def __init__(self, filename):
-        with open(filename) as f:
-            lines = f.readlines()
-        self.model = self.parse(lines)
-
-
-    def parse(self, lines):
-        # Call all the functions (but when to SBML it?)
-        lines = self._strip_list(self._remove_comments(lines))
-
+    def parse(self, path):
+        with open(path, 'r') as f:
+            data = f.read()
+        groups = self.group_data(data)
+        self.process_groups(groups)
 
     def _remove_comments(self, lines):
         return [line.split("#")[0] for line in lines]
 
-    def _strip_list(self, lines):
-        return [line.strip() for line in lines if line.strip()]
+    def _remove_whitespace(self, lines):
+        return ["".join(line.split()) for line in lines if line]
 
-    def get_global_params(self):
+    def clean_lines(self, lines):
+        return self._remove_whitespace(self._remove_comments(lines))
+
+    def group_data(self, data):
+        """Group data into fields with comments and whitespace removed
+
+        returns a list of lists
+        """
+        groups = re.findall(r'@[^@]+', data)
+        groups = [re.split(r'\n', group) for group in groups]
+        groups = [self.clean_lines(group) for group in groups]
+        return groups
+
+    def process_groups(self, groups):
+        """Call the relevent function to process each group"""
+        PROCESSES = {
+            "@init_amounts": self.set_init_amounts,
+            "@array_lvl_params": self.set_array_lvl_params,
+            "@compartement_lvl_params": self.set_compartment_lvl_params,
+            "@reaction": self.set_reaction
+            }
+        [PROCESSES[group[0]](group[1:]) for group in groups]
+
+    def set_init_amounts(self, lines):
+        """Set species and initial amounts"""
+        pattern = re.compile(r"(\w+):(NONE|\w+)")
+
+    def set_array_lvl_params(self, lines):
         pass
 
-    # Need to call before matching reactions with a regex in
-    # get_reactions
-    def _remove_whitespace(self, lines):
-        return ["".join(line.split()) for line in lines]
+    def set_compartment_lvl_params(self, lines):
+        pass
+
+    def set_reaction(self, lines):
+        pass
+
+
+
+    # def _strip_list(self, lines):
+    #     return [line.strip() for line in lines if line.strip()]
+
+    # def read_lines(self, path):
+    #     with open(path, 'r') as f:
+    #         lines = f.readlines()
+    #     return lines
+
 
     def get_reactions(self, lines):
         irreversible_reg = re.compile(r'(?P<reactants>(?:\w+)*(?:\+\w+)*)[-]+>'
@@ -72,7 +108,8 @@ class Parser(object):
                               libsbml.ASTNode)
         except AssertionError:
             raise libsbml.getLastParseL3Error()
-
+        # Could also test whether fun()'s are builtin SBML. I.e. if
+        # we do not want to support custom functions.
 
     def add_species(self):
         pass

2fc65ff116d840e5b72dafb64b04c186eb1806e6
Daniel Boocock <daniel.boocock@protonmail.ch>
2017-02-02 16:06:37 +0000

    Parse init_amounts and species

--- cans/parser.py
+++ cans/parser.py
@@ -5,9 +5,12 @@ import re
 
 class Parser(object):
 
-    def __init__(self, path):
+    def __init__(self, path=""):
         self.path = path
-        self.parse(path)
+        if path:
+            self.parse(path)
+        else:
+            pass    # Could set empty attributes
 
     def parse(self, path):
         with open(path, 'r') as f:
@@ -15,46 +18,71 @@ class Parser(object):
         groups = self.group_data(data)
         self.process_groups(groups)
 
-    def _remove_comments(self, lines):
-        return [line.split("#")[0] for line in lines]
+    # def _remove_comments(self, lines):
+    #     return [line.split("#")[0] for line in lines]
 
-    def _remove_whitespace(self, lines):
-        return ["".join(line.split()) for line in lines if line]
+    # def _remove_whitespace(self, lines):
+    #     return ["".join(line.split()) for line in lines if line]
 
-    def clean_lines(self, lines):
-        return self._remove_whitespace(self._remove_comments(lines))
+    # def _clean_lines(self, lines):
+    #     return self._remove_whitespace(self._remove_comments(lines))
+
+    def _clean_lines(self, lines):
+        """Remove comments and whitespace"""
+        lines = [line.split("#")[0] for line in lines]
+        lines = ["".join(line.split()) for line in lines if line]
+        return lines
 
     def group_data(self, data):
-        """Group data into fields with comments and whitespace removed
-
-        returns a list of lists
-        """
+        """Group data into fields with comments and whitespace removed"""
         groups = re.findall(r'@[^@]+', data)
         groups = [re.split(r'\n', group) for group in groups]
-        groups = [self.clean_lines(group) for group in groups]
+        groups = [self._clean_lines(group) for group in groups]
         return groups
 
     def process_groups(self, groups):
         """Call the relevent function to process each group"""
         PROCESSES = {
-            "@init_amounts": self.set_init_amounts,
-            "@array_lvl_params": self.set_array_lvl_params,
-            "@compartement_lvl_params": self.set_compartment_lvl_params,
-            "@reaction": self.set_reaction
+            "@init_amounts": self.parse_init_amounts,
+            "@array_lvl_params": self.parse_array_lvl_params,
+            "@compartement_lvl_params": self.parse_compartment_lvl_params,
+            "@reaction": self.parse_reaction
             }
         [PROCESSES[group[0]](group[1:]) for group in groups]
 
-    def set_init_amounts(self, lines):
+    def parse_amount(self, species, amount):
+        try:
+            return float(amount)
+        except ValueError:
+            try:
+                assert(amount == "None")
+                return None
+            except AssertionError:
+                msg = ("Warning: amount '{0}' for species '{1}' is not valid. "
+                       "Amount should be either a string representation of "
+                       "a number or 'None'. Returning None.")
+                print(msg.format(amount, species))
+                return None
+
+    def parse_init_amounts(self, lines):
         """Set species and initial amounts"""
-        pattern = re.compile(r"(\w+):(NONE|\w+)")
+        pattern = re.compile(r"(\w+):(.*)")
+        # Could match exactly as below but error handling is easier
+        # when matching anything.
+        # pattern = re.compile(r'(\w+):([-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?|None')
+        matches = [re.match(pattern, line).groups() for line in lines]
+        dct = {}
+        dct["species"] = [s for s, a in matches]
+        dct["init_amounts"] = {s: self.parse_amount(s, a) for s, a in matches}
+        return dct
 
-    def set_array_lvl_params(self, lines):
+    def parse_array_lvl_params(self, lines):
         pass
 
-    def set_compartment_lvl_params(self, lines):
+    def parse_compartment_lvl_params(self, lines):
         pass
 
-    def set_reaction(self, lines):
+    def parse_reactions(self, lines):
         pass
 
 
